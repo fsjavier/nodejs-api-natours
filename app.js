e783f4c9321097390b0dad0361682fa24241501f
fsjavier <javier.fersal@gmail.com>
2025-01-20 16:14:09 +0100

    Add detail get, patch and delete endpoints

--- app.js
+++ app.js
@@ -5,18 +5,6 @@ const app = express();
 
 app.use(express.json());
 
-/*
-app.get("/", (req, res) => {
-  res
-    .status(200)
-    .json({ message: "Hello from the server side!", app: "Natours" });
-});
-
-app.post("/", (req, res) => {
-  res.send("You can post to this endpoint...");
-});
-*/
-
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
@@ -28,6 +16,20 @@ app.get("/api/v1/tours", (req, res) => {
     data: {
       tours, // same as tours: tours
     },
+  });
+});
+
+app.get("/api/v1/tours/:id", (req, res) => {
+  const id = req.params.id * 1;
+  const tour = tours.find((el) => el.id === id);
+
+  if (!tour) {
+    return res.status(404).json({ status: "fail", message: "Invalid ID" });
+  }
+
+  res.status(200).json({
+    status: "success",
+    data: { tour },
   });
 });
 
@@ -43,6 +45,36 @@ app.post("/api/v1/tours", (req, res) => {
       res.status(201).json({ status: "success", data: { tour: newTour } });
     }
   );
+});
+
+app.patch("/api/v1/tours/:id", (req, res) => {
+  const id = req.params.id * 1;
+  const tour = tours.find((el) => el.id === id);
+
+  if (!tour) {
+    return res.status(404).json({ status: "fail", message: "Invalid ID" });
+  }
+
+  res.status(200).json({
+    status: "success",
+    data: {
+      tour: "<Updated tour here...>",
+    },
+  });
+});
+
+app.delete("/api/v1/tours/:id", (req, res) => {
+  const id = req.params.id * 1;
+  const tour = tours.find((el) => el.id === id);
+
+  if (!tour) {
+    return res.status(404).json({ status: "fail", message: "Invalid ID" });
+  }
+
+  res.status(204).json({
+    status: "success",
+    data: null,
+  });
 });
 
 const port = 3000;

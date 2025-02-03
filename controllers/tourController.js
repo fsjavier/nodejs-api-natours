6d7ee0646ad801aaefb97947ec03222d0e412fbc
fsjavier <javier.fersal@gmail.com>
2025-02-03 10:32:39 +0100

    Add tours filtering

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -43,8 +43,33 @@ exports.checkBody = (req, res, next) => {
 
 exports.getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find();
+    /*
+    const tours = await Tour.find({
+      duration: 5,
+      difficulty: "easy"
+    })
+    */
+    /*
+    const tours = await Tour.find()
+      .where("duration")
+      .equals(5)
+      .where("difficulty")
+      .equals("easy");
+    */
 
+    // BUILD QUERY
+    const queryObj = { ...req.query };
+    const excludedFields = ["page", "sort", "limit", "fields"];
+    excludedFields.forEach((el) => delete queryObj[el]);
+
+    console.log(req.query, queryObj);
+
+    const query = Tour.find(queryObj);
+
+    // EXECUTE QUERY
+    const tours = await query;
+
+    // SEND RESPONSE
     res.status(200).json({
       status: "success",
       results: tours.length,

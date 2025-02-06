62c1d0bd91d8c3a1601ce62d63a305d2fe891b02
fsjavier <javier.fersal@gmail.com>
2025-02-06 12:52:02 +0100

    Add error handling and refactor

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const morgan = require("morgan");
 
+const AppError = require("./utils/appError");
+const globalErrorHandler = require("./controllers/errorController");
 const tourRouter = require("./routes/tourRoutes");
 const userRouter = require("./routes/userRoutes");
 
@@ -13,10 +15,10 @@ if (process.env.NODE_ENV === "development") {
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
-app.use((req, res, next) => {
-  console.log("Hello from the Middleware 👋");
-  next();
-});
+// app.use((req, res, next) => {
+//   console.log("Hello from the Middleware 👋");
+//   next();
+// });
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
@@ -27,6 +29,13 @@ app.use((req, res, next) => {
 
 app.use("/api/v1/tours", tourRouter);
 app.use("/api/v1/users", userRouter);
+
+// Handle all other routes for all HTTP methods
+app.all("*", (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
+
+app.use(globalErrorHandler);
 
 // app.get("/api/v1/tours", getAllTours);
 // app.post("/api/v1/tours", createTour);

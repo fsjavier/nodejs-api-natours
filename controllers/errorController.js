2d0af76a8b777ba438379f9224561b9e18a5b28d
fsjavier <javier.fersal@gmail.com>
2025-02-07 11:33:59 +0100

    Handle CastError in production

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,3 +1,10 @@
+const AppError = require("../utils/appError");
+
+const handleCastErrorDB = (err) => {
+  const message = `Invalid ${err.path}: ${err.value}`;
+  return new AppError(message, 400);
+};
+
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -8,18 +15,21 @@ const sendErrorDev = (err, res) => {
 };
 
 const sendErrorProd = (err, res) => {
+  // Operational, trusted error: send message to client
   if (err.isOperational) {
     res.status(err.statusCode).json({
       status: err.status,
       message: err.message,
     });
+
+    // Programming or other unknown error: don't leak error details
   } else {
     // 1) Log error
     console.error("ERROR", err);
 
     // 2) Send generic message to client
     res.status(500).json({
-      status: err,
+      status: "error",
       message: "Something went very wrong!",
     });
   }
@@ -31,9 +41,10 @@ module.exports = (err, req, res, next) => {
 
   if (process.env.NODE_ENV === "development") {
     sendErrorDev(err, res);
-  }
+  } else if (process.env.NODE_ENV === "production") {
+    let error = { ...err };
+    if (error.name === "CastError") error = handleCastErrorDB(error);
 
-  if (process.env.NODE_ENV === "production") {
-    sendErrorProd(err, res);
+    sendErrorProd(error, res);
   }
 };
